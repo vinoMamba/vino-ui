d87fed35eca601ae62883162ae88785e4a210d83
vino <vino0908@outlook.com>
2021-09-25 23:41:49 +0800

    feat: 添加按钮的ico

--- src/lib/button/button.tsx
+++ src/lib/button/button.tsx
@@ -1,11 +1,32 @@
-import {defineComponent} from "vue";
+import {defineComponent, PropType} from "vue";
 import "./style/button.scss";
 
+type  IconPosition = "left" | "right"
+
 export default defineComponent({
-    name: "VButton",
-    setup() {
-        return () => (
-            <button class="v-button">按钮</button>
-        );
+        name: "VButton",
+        props: {
+            icon: {
+                type: String,
+            },
+            iconPosition: {
+                type: String as PropType<IconPosition>,
+                default: "left"
+            }
+        },
+        setup(props, {slots}) {
+            return () => (
+                <button class={{"v-button": true, [`icon-${props.iconPosition}`]: true}}>
+                    {props.icon ?
+                        <svg class="icon">
+                            <use xlinkHref={`#v-${props.icon}`}/>
+                        </svg> : ""
+                    }
+                    <div class="content">
+                        {slots.default ? slots.default() : "确定"}
+                    </div>
+                </button>
+            );
+        }
     }
-});
+);
