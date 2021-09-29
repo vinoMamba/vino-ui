a7e5bc93c1c3aac76b2dba39fde035c2fc1b519b
vino <vino0908@outlook.com>
2021-09-29 21:53:39 +0800

    doc: update README.md

--- README.md
+++ README.md
@@ -1,11 +1,17 @@
-# Vue 3 + Typescript + Vite
+# Vino UI 组件库
 
-This template should help get you started developing with Vue 3 and Typescript in Vite. The template uses Vue 3 `<script setup>` SFCs, check out the [script setup docs](https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup) to learn more.
+[![Build Status](https://app.travis-ci.com/vinoMamba/vino-ui.svg?branch=main)](https://app.travis-ci.com/vinoMamba/vino-ui)
 
-## Recommended IDE Setup
+## 介绍
 
-- [VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=johnsoncodehk.volar)
+## 开始使用
 
-## Type Support For `.vue` Imports in TS
+1. 安装
 
-Since TypeScript cannot handle type information for `.vue` imports, they are shimmed to be a generic Vue component type by default. In most cases this is fine if you don't really care about component prop types outside of templates. However, if you wish to get actual prop types in `.vue` imports (for example to get props validation when using manual `h(...)` calls), you can enable Volar's `.vue` type support plugin by running `Volar: Switch TS Plugin on/off` from VSCode command palette.
+## 文档
+
+## 提问
+
+## 变更记录
+
+## 联系方式
--- src/lib/button/__tests__/button.spec.ts
+++ src/lib/button/__tests__/button.spec.ts
@@ -1,39 +1,18 @@
 import {mount} from "@vue/test-utils";
 import {VButton} from "../../index";
 
-describe('v-button', () => {
+describe("v-button", () => {
 
-    it('可以设置icon', () => {
+    it("可以设置loading", () => {
         const wrapper = mount(VButton, {
             props: {
-                icon: 'setting',
-            }
-        });
-        const useElements = wrapper.vm.$el.querySelectorAll('use');
-        expect(useElements[0].getAttribute('xlink:href')).toEqual('#v-setting');
-    });
-
-    it('可以设置loading', () => {
-        const wrapper = mount(VButton, {
-            props: {
-                icon: 'setting',
+                icon: "setting",
                 loading: true
             }
         });
-        const useElements = wrapper.vm.$el.querySelectorAll('use');
+        const useElements = wrapper.vm.$el.querySelectorAll("use");
         expect(useElements.length).toEqual(1);
-        expect(useElements[0].getAttribute('xlink:href')).toEqual('#v-loading');
+        expect(useElements[0].getAttribute("xlink:href")).toEqual("#v-loading");
     });
-
-    it('可以设置iconPosition', () => {
-        const wrapper = mount(VButton, {
-            props: {
-                icon: 'setting',
-                iconPosition: 'right'
-            }
-        });
-        expect(wrapper.find('button').classes('icon-right')).toBe(true);
-    });
-
 });
 
