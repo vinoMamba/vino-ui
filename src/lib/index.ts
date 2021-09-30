d25afe021862f682da5f2270b512363b8e39eaa4
vino <vino0908@outlook.com>
2021-09-30 21:37:14 +0800

    test travis ci

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,2 +1,3 @@
-export {default as VIcon} from "./icon/Icon";
-export {default as VInput} from "./input/Input";
+export { default as VIcon } from "./icon/Icon";
+export { default as VInput } from "./input/Input";
+export { default as VButton } from "./button/button";
