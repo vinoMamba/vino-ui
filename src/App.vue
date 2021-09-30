aa666e2a1bba52ffddb768712d58c06b988d7c12
vino <vino0908@outlook.com>
2021-09-30 15:54:52 +0800

    feat: 添加button组件样式

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,23 @@
 <template>
-  <div class="wrapper">
+  <h6>input组件</h6>
+  <div class="box">
     <v-input value="李四" />
     <v-input value="禁用" :disabled="true" />
     <v-input value="只读" :readonly="true" />
     <v-input value="error" error="error" />
+  </div>
+  <h6>button组件</h6>
+  <div class="box">
+    <v-button>Click Me</v-button>
+    <v-button theme="button">Click Me</v-button>
+    <v-button theme="link">Click Me</v-button>
+    <v-button theme="text">Click Me</v-button>
+    <v-button size="larger">Click Me</v-button>
+    <v-button size="small">Click Me</v-button>
+    <v-button level="normal">normal</v-button>
+    <v-button level="success">success</v-button>
+    <v-button level="warning">warning</v-button>
+    <v-button level="error">error</v-button>
   </div>
 </template>
 <script lang="ts">
@@ -23,7 +37,7 @@ export default defineComponent({
 </script>
 
 <style scoped>
-.wrapper {
+.box {
   padding: 20px;
 }
 </style>
--- src/lib/button/button.tsx
+++ src/lib/button/button.tsx
@@ -1,19 +1,40 @@
-import {defineComponent} from "vue";
+import { defineComponent, computed } from "vue";
 import "./style/button.css";
 
+const buttonProps = {
+  theme: {
+    type: String,
+    default: "button",
+  },
+  size: {
+    type: String,
+    default: "normal",
+  },
+  level: {
+    type: String,
+    default: "normal",
+  },
+};
 
 const Button = defineComponent({
-        name: "Button",
-        setup(props, {slots}) {
-            return () => (
-                <button class="v-button">
-                    <div class="content">
-                        {slots.default ? slots.default() : "确定"}
-                    </div>
-                </button>
-            );
-        }
-    }
-);
+  name: "Button",
+  props: buttonProps,
+  setup(props, { slots }) {
+    const classes = computed(() => {
+      return {
+        "v-button": true,
+        [`v-theme-${props.theme}`]: true,
+        [`v-size-${props.size}`]: true,
+        [`v-level-${props.level}`]: true,
+      };
+    });
+
+    return () => (
+      <button class={classes.value}>
+        <div class="content">{slots.default ? slots.default() : "确定"}</div>
+      </button>
+    );
+  },
+});
 
 export default Button;
