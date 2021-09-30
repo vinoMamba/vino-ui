a443e2f1412f629d697e267e4458aade9800a8c8
vino <vino0908@outlook.com>
2021-09-30 21:32:54 +0800

    test travis

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,4 +1,2 @@
-export { default as VIcon } from "./icon/Icon";
-export { default as VInput } from "./input/Input";
-
-export { default as VButton } from "./button/Button";
+export {default as VIcon} from "./icon/Icon";
+export {default as VInput} from "./input/Input";
