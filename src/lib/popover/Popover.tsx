445e8a69915c9751bcde9607ad1bb26b2f00efd0
vino <vino0908@outlook.com>
2021-10-14 14:27:37 +0800

    重构代码

--- src/lib/popover/Popover.tsx
+++ src/lib/popover/Popover.tsx
@@ -1,56 +1,58 @@
-import { defineComponent, ref, onUnmounted, onMounted, nextTick } from "vue";
+import { defineComponent, ref, onMounted, nextTick } from "vue";
 import "./style/popover.css";
-import { clickOutsideDiretive, removeListener } from "../utils/clickOutside";
 const Popover = defineComponent({
   name: "Popover",
-  directives: { clickOutside: clickOutsideDiretive },
 
   setup(props, { slots }) {
     const contentWrapper = ref<HTMLElement | null>(null);
     const triggerWrapper = ref<HTMLElement | null>(null);
     const visible = ref(false);
 
-    const toggleVisible = (e: Event) => {
-      e.stopPropagation();
-      visible.value = !visible.value;
-      if (visible.value) {
-        nextTick(() => {
-          const { top, left, width, height } = (
-            triggerWrapper.value as HTMLElement
-          ).getBoundingClientRect();
-          console.log(top, left, height, width);
-          contentWrapper.value!.style.left = left + window.scrollX + "px";
-          contentWrapper.value!.style.top = height + window.scrollY + "px";
-          document.body.appendChild(contentWrapper.value as HTMLElement);
-        });
-      }
+    const positionContent = () => {
+      document.body.appendChild(contentWrapper.value as HTMLElement);
+      const { left, top } = (
+        triggerWrapper.value as HTMLElement
+      ).getBoundingClientRect();
+      contentWrapper.value!.style.left = left + window.scrollX + "px";
+      contentWrapper.value!.style.top = top + window.scrollY + "px";
+    };
+    const listenToDocument = () => {
+      let eventHandle = (e: any) => {
+        if (
+          !contentWrapper.value?.contains(e.target) &&
+          !triggerWrapper.value?.contains(e.target)
+        ) {
+          visible.value = false;
+          document.removeEventListener("click", eventHandle);
+        }
+      };
+      document.addEventListener("click", eventHandle);
     };
 
-    const closePopoverContent = () => {
-      visible.value = false;
+    const onShow = () => {
+      nextTick(() => {
+        positionContent();
+        listenToDocument();
+      });
+    };
+
+    const onclick = (e: any) => {
+      if (triggerWrapper.value?.contains(e.target)) {
+        visible.value = !visible.value;
+        if (visible.value) {
+          onShow();
+        }
+      }
     };
     onMounted(() => {});
-    onUnmounted(() => {
-      removeListener();
-    });
     return () => (
-      <div
-        class="v-popover"
-        onClick={toggleVisible}
-        vClickOutside={closePopoverContent}
-      >
+      <div class="v-popover" onClick={onclick}>
         {visible.value && (
-          <div
-            ref={contentWrapper}
-            class="v-popover-content-wrapper"
-            onClick={(e) => {
-              e.stopPropagation();
-            }}
-          >
-            <span ref={triggerWrapper}>{slots.content && slots.content()}</span>
+          <div ref={contentWrapper} class="v-popover-content-wrapper">
+            <span>{slots.content && slots.content()}</span>
           </div>
         )}
-        {slots.default && slots.default()}
+        <span ref={triggerWrapper}>{slots.default && slots.default()}</span>
       </div>
     );
   },
