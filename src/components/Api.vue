bb4518e126fc334d1f9ee50a6e6ce8035c8b234f
vino <vino0908@outlook.com>
2021-10-26 14:02:29 +0800

    封装Attr组件

---
+++ src/components/Api.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="wrapper">
+    <h4>Attributes</h4>
+    <div class="api">
+      <ul>
+        <li class="api-title">
+          <span>属性</span>
+          <span>说明</span>
+          <span>类型</span>
+          <span>可选值</span>
+          <span>默认值</span>
+        </li>
+        <li v-for="item in apiList" class="api-item">
+          <span>{{ item.attr }}</span>
+          <span>{{ item.notes }}</span>
+          <span>{{ item.type }}</span>
+          <span>{{ item.optionalValue }}</span>
+          <span>{{ item.default }}</span>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
+
+export interface ApiType {
+  attr: string;
+  notes: string;
+  type: string;
+  optionalValue: string;
+  default: string;
+}
+const ApiProps = {
+  apiList: {
+    type: Array as PropType<ApiType[]>,
+  },
+} as const;
+
+export default defineComponent({
+  name: "Api",
+  props: ApiProps,
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.wrapper {
+  border: 1px solid #d9d9d9;
+  border-radius: 8px;
+  padding: 16px;
+  h4 {
+    font-style: italic;
+    font-size: 24px;
+    color: rgba(0, 0, 0, 0.85);
+    font-weight: 600;
+    margin-bottom: 8px;
+    margin-top: 8px;
+  }
+  .api {
+    li {
+      font-size: 14px;
+      padding: 16px 0;
+      display: flex;
+      align-items: center;
+      justify-content: space-around;
+      border-bottom: 1px solid #d9d9d9;
+      color: rgba(0, 0, 0, 0.6);
+
+      &:first-child {
+        font-size: 15px;
+        color: rgba(0, 0, 0, 0.85);
+      }
+      &:last-child {
+        border-bottom: none;
+      }
+
+      span {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 25%;
+      }
+    }
+  }
+}
+</style>
--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,30 +1,60 @@
 <template>
-  <Demo title="按钮 Button" :demo-list="demoList"/>
+  <Demo title="按钮 Button" :demo-list="demoList" />
+  <Api :api-list="apiList" />
 </template>
 <script lang="ts">
-import {defineComponent, ref, shallowRef} from "vue";
+import { defineComponent, ref, shallowRef } from "vue";
+import Api, { ApiType } from "./Api.vue";
 import Button1Demo from "../demo-code/Button1Demo.vue";
 import Button2Demo from "../demo-code/Button2Demo.vue";
 import Button3Demo from "../demo-code/Button3Demo.vue";
 import Button4Demo from "../demo-code/Button4Demo.vue";
 import Button5Demo from "../demo-code/Button5Demo.vue";
-import Demo, {DemoType} from "./Demo.vue";
+import Demo, { DemoType } from "./Demo.vue";
 
 export default defineComponent({
   name: "ButtonDemo",
-  components: {Demo},
+  components: { Demo, Api },
   setup() {
-    const demoList = ref<DemoType[]>(
-        [
-          {codeVisible: false, title: "基础按钮，可以disabled属性", component: shallowRef(Button1Demo)},
-          {codeVisible: false, title: "按钮的theme分别为 normal、text、link", component: shallowRef(Button2Demo)},
-          {codeVisible: false, title: "按钮的size分别为 normal、small、larger", component: shallowRef(Button3Demo)},
-          {codeVisible: false, title: "按钮的 level 分别为 normal、success、warning、error", component: shallowRef(Button4Demo)},
-          {codeVisible: false, title: "按钮可以设置 loading 状态", component: shallowRef(Button5Demo)},
-        ]
-    );
+    const demoList = ref<DemoType[]>([
+      {
+        codeVisible: false,
+        title: "基础按钮，可以disabled属性",
+        component: shallowRef(Button1Demo),
+      },
+      {
+        codeVisible: false,
+        title: "按钮的theme分别为 normal、text、link",
+        component: shallowRef(Button2Demo),
+      },
+      {
+        codeVisible: false,
+        title: "按钮的size分别为 normal、small、larger",
+        component: shallowRef(Button3Demo),
+      },
+      {
+        codeVisible: false,
+        title: "按钮的 level 分别为 normal、success、warning、error",
+        component: shallowRef(Button4Demo),
+      },
+      {
+        codeVisible: false,
+        title: "按钮可以设置 loading 状态",
+        component: shallowRef(Button5Demo),
+      },
+    ]);
+    const apiList = ref<ApiType[]>([
+      {
+        attr: "xxx",
+        type: "String",
+        notes: "说明",
+        optionalValue: "xxxx",
+        default: "xxx",
+      },
+    ]);
     return {
       demoList,
+      apiList,
     };
   },
 });
